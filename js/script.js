bb8c0c3120789a098aab6cc8da663740afa4e808
Nils Kriedner <nkriedner@googlemail.com>
2021-07-17 13:01:20 +0200

    Add gallery and modal content

--- js/script.js
+++ js/script.js
@@ -7,3 +7,40 @@ const searchContainerContent = `<form action="#" method="get">
                             <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
                                 </form>`;
 searchContainer.insertAdjacentHTML("beforeend", searchContainerContent);
+
+// Create gallery content:
+const gallery = document.getElementById("gallery");
+const galleryContent = `<div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="https://via.placeholder.com/90" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">first last</h3>
+                        <p class="card-text">email</p>
+                        <p class="card-text cap">city, state</p>
+                    </div>
+                        </div>`;
+gallery.insertAdjacentHTML("beforeend", galleryContent);
+
+// Create modal content:
+const body = document.querySelector("body");
+const modal = `<div class="modal-container">
+                    <div class="modal">
+                        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                        <div class="modal-info-container">
+                            <img class="modal-img" src="https://via.placeholder.com/125" alt="profile picture">
+                            <h3 id="name" class="modal-name cap">name</h3>
+                            <p class="modal-text">email</p>
+                            <p class="modal-text cap">city</p>
+                            <hr>
+                            <p class="modal-text">(555) 555-5555</p>
+                            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                            <p class="modal-text">Birthday: 10/21/2015</p>
+                        </div>
+                    </div>
+                    <div class="modal-btn-container">
+                        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                    </div>
+                </div>`;
+body.insertAdjacentHTML("beforeend", modal);
