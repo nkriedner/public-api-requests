1021a0a9145cc73e8bb62b03855d3e0de81d4ac0
Nils Kriedner <nkriedner@googlemail.com>
2021-07-17 12:48:25 +0200

    Create search-container content on page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("connected");
+console.log("script.js is connected");
+
+// Create search-container content:
+const searchContainer = document.querySelector(".search-container");
+const searchContainerContent = `<form action="#" method="get">
+                            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+                            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+                                </form>`;
+searchContainer.insertAdjacentHTML("beforeend", searchContainerContent);
